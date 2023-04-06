e1973b55007650c8855491a644d8cc9c9785c92e
Younger Jo <youngerjo@gmail.com>
2023-04-06 10:12:28 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Next DaisyUI Starter
+
+A simple web starter project based on Next.js and DaisyUI
+
+## Installation
+
+```
+$ yarn
+```
+
+## Run
+
+```
+$ yarn dev
+```
+
+## License
+
+MIT
---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+export default function Page() {
+  return (
+    <div className="container mx-auto">
+      <article className="prose py-8">
+        <h1>Hello, world!</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+          soluta explicabo obcaecati, temporibus incidunt quidem libero delectus
+          vel! Adipisci soluta quisquam possimus est in fuga quibusdam mollitia
+          sed dignissimos nobis.
+        </p>
+      </article>
+      <div className="divider" />
+      <button className="btn">Click me!</button>
+    </div>
+  )
+}
